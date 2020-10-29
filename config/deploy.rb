a8ab3fed489dbe6d23af3bf303c3aad4e6ed1b22
Akbar Maulana <akbar.v.maulana@gmail.com>
2020-10-29 22:53:35 +0700

    Add Postgres

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :deploy_to, "/home/deploy/my_app"
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 append :linked_files, "config/database.yml", "config/secrets.yml"
-append :linked_dirs, "bin", "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
