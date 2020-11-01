c0701dd96628613ac40810465de2d4a5997be6a0
Akbar Maulana <akbar.v.maulana@gmail.com>
2020-11-01 13:59:26 +0700

    Add foreman and bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import '../stylesheets/application'
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
+
+var jQuery = require("jquery");
+
+// import jQuery from "jquery";
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
+require("bootstrap");
+require("admin-lte");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,17 @@
-const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
+const { environment } = require("@rails/webpacker");
 
-environment.plugins.prepend('Provide',
+const webpack = require("webpack");
+
+// Add an additional plugin of your choosing : ProvidePlugin
+environment.plugins.prepend(
+  "Provide",
   new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
+    $: "jquery",
+    jQuery: "jquery",
+    jquery: "jquery",
+    "window.Tether": "tether",
+    Popper: ["popper.js", "default"] // for Bootstrap 4
   })
-)
+);
 
-module.exports = environment
+module.exports = environment;
